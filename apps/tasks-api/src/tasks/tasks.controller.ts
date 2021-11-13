2b54a7eed4cb6c2a3bad3e70c322ff76cf0cae97
Kozlov Nikita <kozlovzxc@gmail.com>
2021-11-14 01:36:52 +0300

    feat(tasks-api): basic BE functionality

--- apps/tasks-api/src/tasks/tasks.controller.ts
+++ apps/tasks-api/src/tasks/tasks.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../common/guards/auth.guard';
+import { TasksService } from './tasks.service';
+import { IsString } from 'class-validator';
+
+class CreateDTO {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+}
 
 @Controller('tasks')
+@UseGuards(AuthGuard)
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  @UseGuards(AuthGuard)
   getAll() {
-    return [];
+    return this.tasksService.getAll();
+  }
+
+  @Post()
+  create(@Body() body: CreateDTO) {
+    return this.tasksService.create({
+      title: body.title,
+      description: body.description,
+    });
   }
 }
--- apps/tasks-api/src/tasks/tasks.module.ts
+++ apps/tasks-api/src/tasks/tasks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { AuthGuardModule } from '../common/guards/auth-guard.module';
+import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntity } from './task.entity';
 
 @Module({
-  imports: [AuthGuardModule],
+  imports: [TypeOrmModule.forFeature([TaskEntity]), AuthGuardModule],
+  providers: [TasksService],
   controllers: [TasksController],
 })
 export class TasksModule {}
