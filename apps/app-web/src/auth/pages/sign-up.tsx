43c81100b6fce40177b0073a5c889670a04700f2
Kozlov Nikita <kozlovzxc@gmail.com>
2021-11-13 20:51:05 +0300

    feat(app-web): Styled auth pages

--- apps/app-web/src/auth/pages/sign-up.tsx
+++ apps/app-web/src/auth/pages/sign-up.tsx
@@ -25,34 +25,36 @@ export const SignUp = memo(() => {
   });
 
   return (
-    <>
-      <h1 className={classes.title}>Sign Up</h1>
-      {/* TODO: handle submit errors */}
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <label className={classes.form__label}>
-          Username
-          <br />
-          <input
-            type="text"
-            autoComplete="username"
-            {...register("username")}
-          />
-        </label>
+    <div className={classes.container}>
+      <div>
+        <h1 className={classes.title}>Sign Up</h1>
+        {/* TODO: handle submit errors */}
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <label className={classes.form__label}>
+            Username
+            <br />
+            <input
+              type="text"
+              autoComplete="username"
+              {...register("username")}
+            />
+          </label>
 
-        <label className={classes.form__label}>
-          Password
-          <br />
-          <input
-            type="password"
-            autoComplete="current-password"
-            {...register("password")}
-          />
-        </label>
+          <label className={classes.form__label}>
+            Password
+            <br />
+            <input
+              type="password"
+              autoComplete="current-password"
+              {...register("password")}
+            />
+          </label>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 });
