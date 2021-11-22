b57483e212c75eaafd61dff2b5de61e33ac087f3
Kozlov Nikita <kozlovzxc@gmail.com>
2021-11-22 18:15:51 +0300

    feat(common): added tasks & renamed config

--- apps/tasks/project/settings.py
+++ apps/tasks/project/settings.py
@@ -33,12 +33,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed
+
+    # Custom
+    'accounts',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -88,7 +95,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-# AUTH_USER_MODEL = 'tasks.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
